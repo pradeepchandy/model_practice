b16af9fc318273c362e3ee452dda6c8606e30de9
Pradeep Chandy <pradeepchandy@gmail.com>
2014-05-11 22:27:22 -0500

    v1.0

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,10 @@ class QuestionsController < ApplicationController
 
     # Your Ruby goes here.
 
-    # @most_recent_movie_for_second_actor = ???
+
+   @most_recent_movie_for_second_actor = Actor.second.movies.order("year DESC").first.title
+
+
   end
 
   def question_2
@@ -12,25 +15,36 @@ class QuestionsController < ApplicationController
 
     # Your Ruby goes here.
 
-    # @director_of_longest_movie = ???
+  @director_of_longest_movie = Director.find(Movie.order("duration desc").first.director_id).name
+
   end
 
   def question_3
     # Which director has the most movies on the list?
 
-    # Your Ruby goes here.
+movie_counts = Hash.new
 
-    # @director_with_the_most_movies = ???
+    Director.all.each do |the_director|
+      number_of_movies_directed_by_the_director = the_director.movies.count
+      movie_counts =  movie_counts.merge(the_director.name =>number_of_movies_directed_by_the_director)
+    end
+
+     @director_with_the_most_movies = movie_counts.sort_by{|k,v| v}.reverse.first[0]
   end
 
   def question_4
     # Which actor has been in the most movies on the list?
-    # (If there's a tie, any one of them is fine)
+     movie_counts = Hash.new
 
-    # Your Ruby goes here.
+    Actor.all.each do |the_actor|
+      movie_counts =  movie_counts.merge(the_actor.name =>the_actor.movies.count)
+    end
 
-    # @actor_with_the_most_movies = ???
+     @actor_with_the_most_movies = movie_counts.sort_by{|k,v| v}.reverse.first[0]
   end
+
+
+
 
   def question_5
     # This one is hard. Work on it after all your other review is complete.
@@ -40,8 +54,24 @@ class QuestionsController < ApplicationController
 
     # Your Ruby goes here.
 
-    # @actor = ???
-    # @director = ???
-    # @movies_together = ???
+   movies_for_pair = Hash.new
+
+    Movie.all.each do |the_movie|
+
+       the_movie.actors.all.each do |the_actor|
+
+     movies_for_pair =  movies_for_pair.merge([the_actor.name, the_movie.director.name] => the_movie.title)
+
+end
+end
+
+     @movies_for_pair = movies_for_pair.count
+
+
+
+     #@actor = movies_for_pair[]
+    # @director = sdfd
+    # @movies_together = dfd
+
   end
 end
